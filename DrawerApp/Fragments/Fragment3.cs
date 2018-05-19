e2ce49fc2f5ae5e52750e5a707514d71de4d48fc
hazzii <hasnain.jiwa@student.anglia.ac.uk>
2018-05-19 17:07:34 +0100

    Added third fragment to implement features in.

---
+++ DrawerApp/Fragments/Fragment3.cs
@@ -0,0 +1,28 @@
+﻿using Android.Support.V4.App;
+using Android.OS;
+using Android.Views;
+
+namespace DrawerApp.Fragments
+{
+    public class Fragment3 : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public static Fragment3 NewInstance()
+        {
+            var frag3 = new Fragment3 { Arguments = new Bundle() };
+            return frag3;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var ignored = base.OnCreateView(inflater, container, savedInstanceState);
+            return inflater.Inflate(Resource.Layout.fragment2, null);
+        }
+    }
+}
