a234345bbbd3d5f9c6909cb34a249a56ef3828d2
hazzii <hasnain.jiwa@student.anglia.ac.uk>
2018-05-20 17:03:24 +0100

    Creating WeatherApp functionality from previous project

--- DrawerApp/Resources/Resource.Designer.cs
+++ DrawerApp/Resources/Resource.Designer.cs
@@ -2241,8 +2241,8 @@ namespace DrawerApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f07008b
-			public const int action0 = 2131165323;
+			// aapt resource value: 0x7f07009e
+			public const int action0 = 2131165342;
 			
 			// aapt resource value: 0x7f070064
 			public const int action_bar = 2131165284;
@@ -2265,17 +2265,17 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070041
 			public const int action_bar_title = 2131165249;
 			
-			// aapt resource value: 0x7f070088
-			public const int action_container = 2131165320;
+			// aapt resource value: 0x7f07009b
+			public const int action_container = 2131165339;
 			
 			// aapt resource value: 0x7f070065
 			public const int action_context_bar = 2131165285;
 			
-			// aapt resource value: 0x7f07008f
-			public const int action_divider = 2131165327;
+			// aapt resource value: 0x7f0700a2
+			public const int action_divider = 2131165346;
 			
-			// aapt resource value: 0x7f070089
-			public const int action_image = 2131165321;
+			// aapt resource value: 0x7f07009c
+			public const int action_image = 2131165340;
 			
 			// aapt resource value: 0x7f070003
 			public const int action_menu_divider = 2131165187;
@@ -2292,11 +2292,11 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070043
 			public const int action_mode_close_button = 2131165251;
 			
-			// aapt resource value: 0x7f07008a
-			public const int action_text = 2131165322;
+			// aapt resource value: 0x7f07009d
+			public const int action_text = 2131165341;
 			
-			// aapt resource value: 0x7f070098
-			public const int actions = 2131165336;
+			// aapt resource value: 0x7f0700ab
+			public const int actions = 2131165355;
 			
 			// aapt resource value: 0x7f070044
 			public const int activity_chooser_view_content = 2131165252;
@@ -2325,8 +2325,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f07004b
 			public const int buttonPanel = 2131165259;
 			
-			// aapt resource value: 0x7f07008c
-			public const int cancel_action = 2131165324;
+			// aapt resource value: 0x7f07009f
+			public const int cancel_action = 2131165343;
 			
 			// aapt resource value: 0x7f070030
 			public const int center = 2131165232;
@@ -2340,8 +2340,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f07005b
 			public const int checkbox = 2131165275;
 			
-			// aapt resource value: 0x7f070094
-			public const int chronometer = 2131165332;
+			// aapt resource value: 0x7f0700a7
+			public const int chronometer = 2131165351;
 			
 			// aapt resource value: 0x7f070039
 			public const int clip_horizontal = 2131165241;
@@ -2352,14 +2352,23 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070024
 			public const int collapseActionView = 2131165220;
 			
+			// aapt resource value: 0x7f070094
+			public const int condLabel = 2131165332;
+			
+			// aapt resource value: 0x7f070093
+			public const int condSection = 2131165331;
+			
+			// aapt resource value: 0x7f070095
+			public const int condText = 2131165333;
+			
 			// aapt resource value: 0x7f070075
 			public const int container = 2131165301;
 			
 			// aapt resource value: 0x7f07004e
 			public const int contentPanel = 2131165262;
 			
-			// aapt resource value: 0x7f070086
-			public const int content_frame = 2131165318;
+			// aapt resource value: 0x7f070099
+			public const int content_frame = 2131165337;
 			
 			// aapt resource value: 0x7f070076
 			public const int coordinator = 2131165302;
@@ -2394,8 +2403,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070012
 			public const int disableHome = 2131165202;
 			
-			// aapt resource value: 0x7f070083
-			public const int drawer_layout = 2131165315;
+			// aapt resource value: 0x7f070096
+			public const int drawer_layout = 2131165334;
 			
 			// aapt resource value: 0x7f070066
 			public const int edit_query = 2131165286;
@@ -2403,8 +2412,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070021
 			public const int end = 2131165217;
 			
-			// aapt resource value: 0x7f07009e
-			public const int end_padder = 2131165342;
+			// aapt resource value: 0x7f0700b1
+			public const int end_padder = 2131165361;
 			
 			// aapt resource value: 0x7f07002a
 			public const int enterAlways = 2131165226;
@@ -2433,17 +2442,32 @@ namespace DrawerApp
 			// aapt resource value: 0x7f07003f
 			public const int @fixed = 2131165247;
 			
+			// aapt resource value: 0x7f070088
+			public const int getSection = 2131165320;
+			
+			// aapt resource value: 0x7f070089
+			public const int getWeatherButton = 2131165321;
+			
 			// aapt resource value: 0x7f070005
 			public const int home = 2131165189;
 			
 			// aapt resource value: 0x7f070013
 			public const int homeAsUp = 2131165203;
 			
+			// aapt resource value: 0x7f070091
+			public const int humidLabel = 2131165329;
+			
+			// aapt resource value: 0x7f070090
+			public const int humidSection = 2131165328;
+			
+			// aapt resource value: 0x7f070092
+			public const int humidText = 2131165330;
+			
 			// aapt resource value: 0x7f070049
 			public const int icon = 2131165257;
 			
-			// aapt resource value: 0x7f070099
-			public const int icon_group = 2131165337;
+			// aapt resource value: 0x7f0700ac
+			public const int icon_group = 2131165356;
 			
 			// aapt resource value: 0x7f070025
 			public const int ifRoom = 2131165221;
@@ -2451,8 +2475,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070046
 			public const int image = 2131165254;
 			
-			// aapt resource value: 0x7f070095
-			public const int info = 2131165333;
+			// aapt resource value: 0x7f0700a8
+			public const int info = 2131165352;
 			
 			// aapt resource value: 0x7f070000
 			public const int item_touch_helper_previous_elevation = 2131165184;
@@ -2460,14 +2484,23 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070074
 			public const int largeLabel = 2131165300;
 			
+			// aapt resource value: 0x7f070083
+			public const int latLabel = 2131165315;
+			
+			// aapt resource value: 0x7f070082
+			public const int latSection = 2131165314;
+			
+			// aapt resource value: 0x7f070084
+			public const int latText = 2131165316;
+			
 			// aapt resource value: 0x7f070034
 			public const int left = 2131165236;
 			
-			// aapt resource value: 0x7f07009a
-			public const int line1 = 2131165338;
+			// aapt resource value: 0x7f0700ad
+			public const int line1 = 2131165357;
 			
-			// aapt resource value: 0x7f07009c
-			public const int line3 = 2131165340;
+			// aapt resource value: 0x7f0700af
+			public const int line3 = 2131165359;
 			
 			// aapt resource value: 0x7f07000f
 			public const int listMode = 2131165199;
@@ -2475,11 +2508,29 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070048
 			public const int list_item = 2131165256;
 			
-			// aapt resource value: 0x7f0700a0
-			public const int masked = 2131165344;
+			// aapt resource value: 0x7f07008b
+			public const int locLabel = 2131165323;
 			
-			// aapt resource value: 0x7f07008e
-			public const int media_actions = 2131165326;
+			// aapt resource value: 0x7f07008a
+			public const int locSection = 2131165322;
+			
+			// aapt resource value: 0x7f07008c
+			public const int locationText = 2131165324;
+			
+			// aapt resource value: 0x7f070086
+			public const int longLabel = 2131165318;
+			
+			// aapt resource value: 0x7f070085
+			public const int longSection = 2131165317;
+			
+			// aapt resource value: 0x7f070087
+			public const int longText = 2131165319;
+			
+			// aapt resource value: 0x7f0700b3
+			public const int masked = 2131165363;
+			
+			// aapt resource value: 0x7f0700a1
+			public const int media_actions = 2131165345;
 			
 			// aapt resource value: 0x7f070022
 			public const int middle = 2131165218;
@@ -2490,17 +2541,17 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070019
 			public const int multiply = 2131165209;
 			
-			// aapt resource value: 0x7f0700a1
-			public const int nav_home_1 = 2131165345;
+			// aapt resource value: 0x7f0700b4
+			public const int nav_home_1 = 2131165364;
 			
-			// aapt resource value: 0x7f0700a2
-			public const int nav_home_2 = 2131165346;
+			// aapt resource value: 0x7f0700b5
+			public const int nav_home_2 = 2131165365;
 			
-			// aapt resource value: 0x7f0700a3
-			public const int nav_home_3 = 2131165347;
+			// aapt resource value: 0x7f0700b6
+			public const int nav_home_3 = 2131165366;
 			
-			// aapt resource value: 0x7f070087
-			public const int nav_view = 2131165319;
+			// aapt resource value: 0x7f07009a
+			public const int nav_view = 2131165338;
 			
 			// aapt resource value: 0x7f07007b
 			public const int navigation_header_container = 2131165307;
@@ -2514,14 +2565,14 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070010
 			public const int normal = 2131165200;
 			
-			// aapt resource value: 0x7f070097
-			public const int notification_background = 2131165335;
+			// aapt resource value: 0x7f0700aa
+			public const int notification_background = 2131165354;
 			
-			// aapt resource value: 0x7f070091
-			public const int notification_main_column = 2131165329;
+			// aapt resource value: 0x7f0700a4
+			public const int notification_main_column = 2131165348;
 			
-			// aapt resource value: 0x7f070090
-			public const int notification_main_column_container = 2131165328;
+			// aapt resource value: 0x7f0700a3
+			public const int notification_main_column_container = 2131165347;
 			
 			// aapt resource value: 0x7f070037
 			public const int parallax = 2131165239;
@@ -2544,11 +2595,11 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070035
 			public const int right = 2131165237;
 			
-			// aapt resource value: 0x7f070096
-			public const int right_icon = 2131165334;
+			// aapt resource value: 0x7f0700a9
+			public const int right_icon = 2131165353;
 			
-			// aapt resource value: 0x7f070092
-			public const int right_side = 2131165330;
+			// aapt resource value: 0x7f0700a5
+			public const int right_side = 2131165349;
 			
 			// aapt resource value: 0x7f07001a
 			public const int screen = 2131165210;
@@ -2643,8 +2694,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070036
 			public const int start = 2131165238;
 			
-			// aapt resource value: 0x7f07008d
-			public const int status_bar_latest_event_content = 2131165325;
+			// aapt resource value: 0x7f0700a0
+			public const int status_bar_latest_event_content = 2131165344;
 			
 			// aapt resource value: 0x7f07005e
 			public const int submenuarrow = 2131165278;
@@ -2655,11 +2706,20 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070011
 			public const int tabMode = 2131165201;
 			
-			// aapt resource value: 0x7f07009d
-			public const int text = 2131165341;
+			// aapt resource value: 0x7f07008e
+			public const int tempLabel = 2131165326;
 			
-			// aapt resource value: 0x7f07009b
-			public const int text2 = 2131165339;
+			// aapt resource value: 0x7f07008d
+			public const int tempSection = 2131165325;
+			
+			// aapt resource value: 0x7f07008f
+			public const int tempText = 2131165327;
+			
+			// aapt resource value: 0x7f0700b0
+			public const int text = 2131165360;
+			
+			// aapt resource value: 0x7f0700ae
+			public const int text2 = 2131165358;
 			
 			// aapt resource value: 0x7f070052
 			public const int textSpacerNoButtons = 2131165266;
@@ -2670,9 +2730,6 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070081
 			public const int textView1 = 2131165313;
 			
-			// aapt resource value: 0x7f070082
-			public const int textView2 = 2131165314;
-			
 			// aapt resource value: 0x7f070080
 			public const int text_input_password_toggle = 2131165312;
 			
@@ -2682,8 +2739,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f07000d
 			public const int textinput_error = 2131165197;
 			
-			// aapt resource value: 0x7f070093
-			public const int time = 2131165331;
+			// aapt resource value: 0x7f0700a6
+			public const int time = 2131165350;
 			
 			// aapt resource value: 0x7f07004a
 			public const int title = 2131165258;
@@ -2694,11 +2751,11 @@ namespace DrawerApp
 			// aapt resource value: 0x7f070057
 			public const int title_template = 2131165271;
 			
-			// aapt resource value: 0x7f070085
-			public const int toolbar = 2131165317;
+			// aapt resource value: 0x7f070098
+			public const int toolbar = 2131165336;
 			
-			// aapt resource value: 0x7f070084
-			public const int toolbar_layout = 2131165316;
+			// aapt resource value: 0x7f070097
+			public const int toolbar_layout = 2131165335;
 			
 			// aapt resource value: 0x7f070029
 			public const int top = 2131165225;
@@ -2724,8 +2781,8 @@ namespace DrawerApp
 			// aapt resource value: 0x7f07000e
 			public const int view_offset_helper = 2131165198;
 			
-			// aapt resource value: 0x7f07009f
-			public const int visible = 2131165343;
+			// aapt resource value: 0x7f0700b2
+			public const int visible = 2131165362;
 			
 			// aapt resource value: 0x7f070027
 			public const int withText = 2131165223;
